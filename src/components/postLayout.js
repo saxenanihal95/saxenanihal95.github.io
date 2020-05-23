3747691f9247fe91d7a7973c719abdd714dab05c
Nihal Saxena <saxenanihal95@gmail.com>
2020-05-23 10:27:13 +0530

    Added optional chaining and syntax highlightling

--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -3,14 +3,40 @@ import { graphql } from "gatsby"
 import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import { Link } from "gatsby"
+// import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live"
+import Highlight, { defaultProps } from "prism-react-renderer"
 
-const shortcodes = { Link } // Provide common components here
+const components = {
+  Link,
+  pre: props => {
+    const language = props?.children?.props?.className?.split("-")[1] || ""
+    return (
+      <Highlight
+        {...defaultProps}
+        code={props.children.props.children}
+        language={language}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    )
+  },
+} // Provide common components here
 
 export default function PageTemplate({ data: { mdx } }) {
   return (
     <div>
       <h1>{mdx.frontmatter.title}</h1>
-      <MDXProvider components={shortcodes}>
+      <MDXProvider components={components}>
         <MDXRenderer>{mdx.body}</MDXRenderer>
       </MDXProvider>
     </div>
